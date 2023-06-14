680bf9b59611d1938860058a6f4e44f509df12a8
arifulhaque-wp <arifulhaque.wp@gmail.com>
2023-06-14 23:38:09 +0600

    title update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once "db.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>My Area Portal</title>
+    <title>Business Area Portal</title>
 
     <link rel="shortcut icon" href="assets/images/favicon/favicon.ico" type="image/x-icon">
 
